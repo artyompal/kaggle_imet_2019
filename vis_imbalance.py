9667f6ab30486d80e337da9676fd6e540be2d4e6
artyom <cppg@cppg.co.uk>
2019-04-07 22:29:43 +0100

    added a little EDA, added a script with TTA

---
+++ vis_imbalance.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3.6
+
+import pandas as pd
+import matplotlib.pyplot as plt
+from collections import Counter
+from debug import dprint
+
+
+df = pd.read_csv('input/train.csv')
+cls_counts = Counter(cls for classes in df['attribute_ids'].str.split() for cls in classes)
+
+labels = pd.read_csv('input/labels.csv')
+labels = labels.to_dict()['attribute_name']
+counts = {labels[int(id)]:count for (id, count) in cls_counts.items()}
+# dprint(counts)
+
+# plt.plot(sorted(counts.values()))
+# plt.show()
+
+dprint(len([cnt for id,cnt in counts.items() if cnt < 100]))
+dprint(len([cnt for id,cnt in counts.items() if cnt < 20]))
+dprint(len([cnt for id,cnt in counts.items() if cnt < 10]))
+dprint(len([cnt for id,cnt in counts.items() if cnt < 5]))
+dprint(len([cnt for id,cnt in counts.items() if cnt < 3]))
+
+# plt.plot(sorted([cnt for id,cnt in counts.items() if cnt < 100])); plt.show()
+
